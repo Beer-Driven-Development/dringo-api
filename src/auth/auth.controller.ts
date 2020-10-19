962ad8bc85b54713066a1d4285f8d650cfdb14e9
Ewelina Lasowy <ewelinalasowy@wp.pl>
2020-10-19 15:06:01 +0200

    add Facebook auth closes #2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,16 +9,21 @@ import {
   ClassSerializerInterceptor,
   Get,
   Req,
+  HttpStatus,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { User } from '../users/entities/user.entity';
 import { AuthService } from './auth.service';
 import { LoginRequestDto } from './dto/login-request.dto';
+import { REQUEST } from '@nestjs/core';
+import { GetUser } from 'src/users/user.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+    
+    ) {}
 
   @Post('login')
   @UseInterceptors(ClassSerializerInterceptor)
@@ -42,4 +47,23 @@ export class AuthController {
   googleAuthRedirect(@Req() req) {
     return this.authService.googleLogin(req);
   }
+
+ 
+  @Get("/facebook")
+  @UseGuards(AuthGuard("facebook"))
+  async facebookLogin(): Promise<any> {
+    return HttpStatus.OK;
+  }
+
+  @Get("/facebook/redirect")
+  @UseGuards(AuthGuard("facebook"))
+  async facebookLoginRedirect(@Req() req: Request, @GetUser() user): Promise<any> {
+    return {
+      statusCode: HttpStatus.OK,
+      data: user
+    };
+  }
+
+
+
 }

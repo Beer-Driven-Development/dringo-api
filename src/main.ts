c9cac3f242938a377749d439309b53156d0f5fdd
MattViper <mattoshinigami@gmail.com>
2020-10-26 17:53:44 +0100

    enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useWebSocketAdapter(new WsAdapter(app));
+  app.enableCors();
   await app.listen(process.env.PORT || 5000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }

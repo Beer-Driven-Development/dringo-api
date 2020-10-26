e77edb07410cef0a780be2801db5a7ad329f7d23
MattViper <mattoshinigami@gmail.com>
2020-10-26 15:41:30 +0100

    little fix to port

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(process.env.APP_PORT || 5000);
+  await app.listen(process.env.PORT || 5000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
